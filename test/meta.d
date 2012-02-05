b5cedabde268b1bd51cab60c333c9075d8aebf27
jpike <jpike@chilon.net>
2012-02-05 18:08:26 +0000

    meta.map

--- test/meta.d
+++ test/meta.d
@@ -2,6 +2,7 @@ module beard.test.meta;
 
 import beard.meta.find;
 import beard.meta.contains;
+import beard.meta.map;
 import beard.io;
 
 template isFloat(T) {
@@ -12,12 +13,23 @@ template isFloatOrBool(T) {
    enum isFloatOrBool = is(T == float) || is(T == bool);
 }
 
+template floatOrBoolToVoid(T) {
+   static if(isFloatOrBool!T)
+       alias void floatOrBoolToVoid;
+   else
+       alias T floatOrBoolToVoid;
+}
+
 int main() {
     println(typeid(find!(isFloat, bool, bool, bool, float)));
     println(typeid(find!(isFloat, bool, bool, int, int)));
     println(containsMatch!(isFloat, bool, bool, bool, float));
     println(containsMatch!(isFloat, bool, bool, int, int));
+
     println(typeid(findAll!(isFloatOrBool, int, bool, double, float, float, int)));
     println(typeid(filter!(isFloatOrBool, int, bool, double, float, float, int)));
+
+    println(typeid(
+        map!(floatOrBoolToVoid, float, bool, double, bool, int, bool, float)));
     return 0;
 }
